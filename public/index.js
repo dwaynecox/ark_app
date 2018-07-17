85191e72ad845dabc75282a1b305760a248d66c7
dwayne <dwayneedwardcox@gmail.com>
2018-07-17 15:29:34 -0500

    after arks display in vue need to tshoot pics

--- public/index.js
+++ public/index.js
@@ -4,13 +4,22 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to the ARK of the Whammynet!!!"
+      arks: [],
+      currentArk: {},
+      message: "Welcome to the ARK of the Whammynet!!!                                                        If you have done or want to do an act of real or random kindness an ARK if you will, you're in the right place! If you'd like a dollar to give to the recipient of your act of kindness to see how your good karma continues...                                                                              MVP will not require this, dollars to be handed out to want to be do gooders...  later... add address ...  TO DO: Button for ARK ideas and display of completed arks... "
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/arks").then(function(response){
+      this.arks = response.data;
+      console.log(this.arks);
+    }.bind(this));
+  },
   methods: {},
   computed: {}
 };
+
+
 
 var SignupPage = {
   template: "#signup-page",
